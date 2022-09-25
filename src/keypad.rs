072ca20148e4ebacdeebec1cb5edcf266b4a4afd
Jacob Gonzalez <jgonzalez@atlassian.com>
2022-09-25 14:40:32 +1000

    I2c (#1)
    * ic2 init
    
    * buttons working
    
    * use u16 for button presses
    
    * moved i2c into keypad struct

---
+++ src/keypad.rs
@@ -0,0 +1,44 @@
+use embassy_rp::{i2c::{I2c, Async, Instance}, peripherals::I2C0};
+use bitvec::prelude::*;
+
+pub struct Keypad {
+    i2c: I2c<'static, I2C0, Async>,
+    prev: u16,
+    current: u16,
+}
+
+impl Keypad {
+    pub fn new(i2c: I2c<'static, I2C0, Async>) -> Self {
+      Self {
+        i2c,
+        current: 0,
+        prev: 0,
+      }
+    }
+
+    pub async fn update(&mut self) {
+        let buttons_buffer: [u8; 2] = Keypad::read_buttons(&mut self.i2c, &[0]).await;
+
+        // bit masking yay. turn 2 u8s into 1 u16
+        self.current = !((buttons_buffer[0] as u16) | ((buttons_buffer[1] as u16) << 8));
+        let changed = self.current ^ self.prev;
+        let _pressed = self.current & changed;
+        self.prev = self.current;
+    }
+
+    pub fn pressed(&self) -> &BitSlice<u16, Lsb0> {
+        self.current.view_bits::<Lsb0>()
+    }
+
+    async fn read_buttons<T: Instance, const N: usize, const M: usize>(
+        i2c: &mut embassy_rp::i2c::I2c<'_, T, Async>,
+        offset: &[u8; N],
+    ) -> [u8; M] {
+        let button_address = 0x20;
+    
+        let mut button_buffer = [0u8; M];
+        i2c.write_read(button_address, offset, &mut button_buffer).await.unwrap();
+    
+        button_buffer
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,66 @@
 #![feature(type_alias_impl_trait)]
 
 use embassy_executor::Spawner;
+use embassy_rp::gpio::Level;
+use embassy_rp::gpio::Output;
+use embassy_rp::spi::Spi;
 use embassy_time::{Duration, Timer};
+
 use rand::rngs::SmallRng;
 use rand::RngCore;
 use rand::SeedableRng;
+use rgb_deck::Keypad;
 use rgb_deck::{Rgb, NUM_LEDS};
+
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
-    let mut rgb = Rgb::new(p);
+    let miso = p.PIN_16;
+    let mosi = p.PIN_19;
+    let clk = p.PIN_18;
+
+    let spi = Spi::new(
+        p.SPI0,
+        clk,
+        mosi,
+        miso,
+        p.DMA_CH0,
+        p.DMA_CH1,
+        embassy_rp::spi::Config::default(),
+    );
+    let cs = Output::new(p.PIN_17, Level::High);
+
+    let mut rgb = Rgb::new(spi, cs);
     rgb.set_brightness(0.5);
 
-    let mut small_rng = SmallRng::seed_from_u64(42);
+    let mut config = embassy_rp::i2c::Config::default();
+    config.frequency = 400_000;
 
-    let mut lit_led = rand(&mut small_rng, NUM_LEDS);
+    let i2c = embassy_rp::i2c::I2c::new(p.I2C0, p.PIN_5, p.PIN_4, p.DMA_CH3, p.DMA_CH4, config);
+    let mut keypad = Keypad::new(i2c);
+
+    let mut _small_rng = SmallRng::seed_from_u64(42);
+
     loop {
+        Timer::after(Duration::from_millis(20)).await;
+
         rgb.update().await;
-        Timer::after(Duration::from_millis(200)).await;
-
-        rgb.set_color(lit_led, 20, 20, 20);
-
-        lit_led = rand(&mut small_rng, NUM_LEDS);
-
-        rgb.set_color(
-            lit_led,
-            rand(&mut small_rng, 255).try_into().unwrap(),
-            rand(&mut small_rng, 255).try_into().unwrap(),
-            rand(&mut small_rng, 255).try_into().unwrap(),
-        );
+        keypad.update().await;
+    
+        
+        for (i, pressed) in keypad.pressed().iter().enumerate() {
+            if *pressed {
+                rgb.set_color(i, 200, 0, 0);
+            } else {
+                rgb.set_color(i, 0, 0, 200);
+            }
+        }
     }
 }
+
 
 fn rand(small_rng: &mut SmallRng, n: usize) -> usize {
     (small_rng.next_u64() % (n as u64)) as usize
